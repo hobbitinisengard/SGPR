268f41de3fbe6e1d692b66242cb9519040010067
viatrufka <bern.zywicki@gmail.com>
2025-01-06 15:23:16 +0100

    wheel fixes

--- Assets/Scripts/Drivetrain/Wheel.cs
+++ Assets/Scripts/Drivetrain/Wheel.cs
@@ -467,7 +467,8 @@ namespace RVP
 			//RaycastHit[] wheelHits = Physics.RaycastAll(transform.position, suspensionParent.springDirection, castDist, RaceManager.I.wheelCastMask);
 			//bool validHit = Physics.SphereCast(transform.position, actualRadius, suspensionParent.springDirection, out RaycastHit hit, castDist, RaceManager.I.wheelCastMask);
 			bool validHit = Physics.Raycast(transform.position, suspensionParent.springDirection, out RaycastHit hit, castDist, RaceManager.I.wheelCastMask);
-
+			if(!validHit)
+				validHit = Physics.Raycast(transform.position, vp.tr.forward, out hit, castDist, RaceManager.I.wheelCastMask);
 			//bool validHit = Physics.BoxCast(transform.position, new Vector3(.05f,.1f,.05f), suspensionParent.springDirection, out RaycastHit hit, 
 			//	 vp.tr.rotation, castDist, RaceManager.I.wheelCastMask);
 			//bool validHit = false;
